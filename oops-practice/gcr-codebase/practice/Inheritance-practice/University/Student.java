b12c5b8f4b3fb30b57430e2b6d1852f1d9396e36
Arpit-kumar-Dubey <arpitkumardubey04@gmail.com>
2026-06-26 19:59:30 +0530

    [Arpit Kumar Dubey].Add Commiting files related to oops-practice probleam

---
+++ oops-practice/gcr-codebase/practice/Inheritance-practice/University/Student.java
@@ -0,0 +1,24 @@
+package University;
+
+public class Student extends Person{
+
+    protected int studentId;
+    protected double studentGpa;
+
+
+    public Student(int id, String name, int studentId, double studentGpa) {
+        super(id, name);
+        this.studentId = studentId;
+        this.studentGpa = studentGpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentGpa=" + studentGpa +
+                ", personId=" + personId +
+                ", personName='" + personName + '\'' +
+                '}';
+    }
+}
